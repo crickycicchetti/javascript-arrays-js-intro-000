03546b6cc1e92b24898f6db1877b69df42627488
Cricky Cicchetti <cricky@flatironschool.com>
2018-05-25 20:02:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,7 @@ var array1 = [];
 
 function addElementToBeginningOfArray(array1, element){
   array1[1] = element
-  return array1
+  return array1;
 }
 
 function destructivelyAddElementToBeginningOfArray(chocolateBars,){
